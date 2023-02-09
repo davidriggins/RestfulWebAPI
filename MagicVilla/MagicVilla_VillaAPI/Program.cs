3dc1f0ca9973d8bb4d0bc924bb9a0911a257f6c2
David Riggins <driggins@esri.com>
2023-02-09 15:10:07 -0800

    Lesson 90 End. User Controller.

--- MagicVilla/MagicVilla_VillaAPI/Program.cs
+++ MagicVilla/MagicVilla_VillaAPI/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 // DI for Villa and VillaNumber Repositories
 builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 
 // Add AutoMapper service
